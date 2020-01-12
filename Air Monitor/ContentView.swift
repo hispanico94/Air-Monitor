5475078e2608cc1666accff4fafe483cf5ebe79f
Paolo Rocca <hispanico94@gmail.com>
2020-01-12 18:22:18 +0100

    Initial Commit

---
+++ Air Monitor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Air Monitor
+//
+//  Created by Paolo Rocca on 12/01/2020.
+//  Copyright © 2020 Paolo Rocca. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
